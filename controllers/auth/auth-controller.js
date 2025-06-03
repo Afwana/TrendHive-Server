9ed285e96c19584224a8e90bee160bf3566bbcba
Afwana <136050247+Afwana@users.noreply.github.com>
2025-06-03 15:41:07 +0530

    Update auth-controller.js

--- controllers/auth/auth-controller.js
+++ controllers/auth/auth-controller.js
@@ -71,7 +71,7 @@ const loginUser = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: production,
+      secure: "production",
       sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
