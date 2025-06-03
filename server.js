be7c9761ea321c269ffdcc71c6faf9a91c8e91b3
Afwana <136050247+Afwana@users.noreply.github.com>
2025-06-03 22:33:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -38,7 +38,8 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://trend-hive-web.vercel.app"],
+    // origin: ["http://localhost:5173", "https://trend-hive-web.vercel.app"],
+    origin: "*",
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
