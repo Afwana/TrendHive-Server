afbdefd5d13ee7ceba150f16c3927f553cb896b5
Afwana <136050247+Afwana@users.noreply.github.com>
2025-06-15 19:39:47 +0530

    Update footer-routes.js

--- routes/admin/footer-routes.js
+++ routes/admin/footer-routes.js
@@ -13,7 +13,7 @@ const router = express.Router();
 
 router.post("/add", addAddress);
 router.get("/get/info", getAdminInfo);
-router.post("/AddLinks", addSocialMediaLinks);
+router.post("/addLinks", addSocialMediaLinks);
 router.get("/mediaLinks", getMediaLinks);
 router.get("/get", fetchAllMessages);
 router.delete("/delete/:id", deleteMessage);
