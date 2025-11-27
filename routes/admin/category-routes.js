6a2188c8dee105c83bb7c6969437829bff9437ce
Afwana <136050247+Afwana@users.noreply.github.com>
2025-11-27 22:04:34 +0530

    Update category-routes.js

--- routes/admin/category-routes.js
+++ routes/admin/category-routes.js
@@ -20,11 +20,10 @@ router.get("/get", fetchAllCategories);
 router.get("/:id", getCategoryById);
 router.put("/edit/:id", editCategory);
 router.delete("/delete/:id", deleteCategory);
-
-// Subcategory routes
 router.get("/:id/subCategories", getSubCategoriesByCategoryId);
-router.post("/:categoryId/subcategories", addSubCategory);
-router.put("/:categoryId/subcategories/:subCategoryId", editSubCategory);
-router.delete("/:categoryId/subcategories/:subCategoryId", deleteSubCategory);
+
+// router.post("/:categoryId/subcategories", addSubCategory);
+// router.put("/:categoryId/subcategories/:subCategoryId", editSubCategory);
+// router.delete("/:categoryId/subcategories/:subCategoryId", deleteSubCategory);
 
 module.exports = router;
